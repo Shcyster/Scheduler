1f2cf440698922dca25c695dfd0ca031fbc74304
Cheyenne <catapour@ucsd.edu>
2016-05-09 17:50:51 -0700

    First two options work with multiple digits. NTF spaces in task description

--- sched.cpp
+++ sched.cpp
@@ -14,6 +14,8 @@ struct widget_t {
 
 int main()
 {
+	vector<string> arr; // array of user's to-do items
+
 	while (1){ // main loop of program
 	
 	// do everything after this	
@@ -27,31 +29,42 @@ int main()
 
 
 	int command; // store user input
-	vector<string> arr; // array of user's to-do items
+	
+
+	string numDays;
+	string taskDescr;
+	string fullTaskDescr;
+	string element;
 
 	cout << "\nMain Menu\n1) Display Schedule\n2) Add Item\n3) Remove Item\n4) Update Item\n" << endl;
 	cin >> command;
-	cout << command;
 
 	switch ( command ) 
 	{
 		case 1:
 		    for(int i = 0; i < arr.size(); i++)
-		    	cout << arr[i] << endl;
+		    	cout << arr[i] << endl; // print first element of string, then rest of string
 			break;
 		case 2:
-			string numDays;
-			string taskDescr;
+			
 			cout << "Input number of days left to complete task: " << endl;
-			cin >> element;
+			cin >> numDays;
 			cout << "Enter Description of Task" << endl;
 			cin >> taskDescr;
-			string element = numDays + taskDescr;
+			/*while (taskDescr != "\n")
+			{
+				fullTaskDescr = fullTaskDescr + taskDescr + " ";
+				cin >> taskDescr;
+			} // while input not null*/
+			element = "(" + numDays + ") " + taskDescr;
 			arr.push_back(element);
+			cout << "Element added\n" << endl; 
 			break;
 		case 3:
+			//cout << "hi" << endl;
 			break;
-		case 4;
+		case 4:
+			// case 4
 			break;
 		default:
 			cout << "Error: Please supply a correct integer between 1 and 4.\n" << endl;
