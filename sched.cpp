0eefd861db2632ea46b35ef75ffc50d18995f4cc
shcyster <cheyenneatapour@aol.com>
2016-05-09 17:01:56 -0700

    Add files via upload

---
+++ sched.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <iostream>
+
+
+using namespace std;
+
+struct widget_t {
+	double x;
+	double y;
+	int count;
+};
+
+
+int main()
+{
+	while (1){ // main loop of program
+	
+	// do everything after this	
+
+
+
+
+
+
+
+
+	
+	int command; // store user input
+
+	cout << "\nMain Menu\n1) Display Schedule\n2) Add Item\n3) Remove Item\n4) Update Item\n" << endl;
+	cin >> command;
+	cout << command;
+
+	switch ( command ) 
+	{
+		case 1:
+
+	} // switch; determine user action
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	} // while main loop of program
+} // main()
