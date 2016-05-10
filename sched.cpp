6262ac0690ed9396a5e0b33b5512823b7d67bd90
Cheyenne <catapour@ucsd.edu>
2016-05-09 17:29:48 -0700

    updated first 2 cases

--- sched.cpp
+++ sched.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
-
+#include <string>
+#include <vector>
 
 using namespace std;
 
@@ -24,8 +25,9 @@ int main()
 
 
 
-	
+
 	int command; // store user input
+	vector<string> arr; // array of user's to-do items
 
 	cout << "\nMain Menu\n1) Display Schedule\n2) Add Item\n3) Remove Item\n4) Update Item\n" << endl;
 	cin >> command;
@@ -34,7 +36,25 @@ int main()
 	switch ( command ) 
 	{
 		case 1:
-
+		    for(int i = 0; i < arr.size(); i++)
+		    	cout << arr[i] << endl;
+			break;
+		case 2:
+			string numDays;
+			string taskDescr;
+			cout << "Input number of days left to complete task: " << endl;
+			cin >> element;
+			cout << "Enter Description of Task" << endl;
+			cin >> taskDescr;
+			string element = numDays + taskDescr;
+			arr.push_back(element);
+			break;
+		case 3:
+			break;
+		case 4;
+			break;
+		default:
+			cout << "Error: Please supply a correct integer between 1 and 4.\n" << endl;
 	} // switch; determine user action
 
 
